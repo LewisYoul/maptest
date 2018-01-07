a8866b48d33c3c5c13412daae171588ae450883b
Lewis <lewisyoul@gmail.com>
2018-01-07 16:13:29 +0000

    add navigation controls to map

--- bundle.js
+++ bundle.js
@@ -71,9 +71,14 @@ var mapboxgl = __webpack_require__(1);
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibGV3aXN5b3VsIiwiYSI6ImNqYzM3a3lndjBhOXQyd24zZnVleGh3c2kifQ.qVH2-BA02t3p62tG72-DZA';
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v10'
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v10',
+  center: ['-0.07', '51.5'],
+  zoom: 9
 });
+
+map.addControl(new mapboxgl.NavigationControl());
+console.log(map)
 
 
 /***/ }),
--- map.js
+++ map.js
@@ -2,6 +2,11 @@ var mapboxgl = require('./node_modules/mapbox-gl/dist/mapbox-gl.js');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibGV3aXN5b3VsIiwiYSI6ImNqYzM3a3lndjBhOXQyd24zZnVleGh3c2kifQ.qVH2-BA02t3p62tG72-DZA';
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v10'
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v10',
+  center: ['-0.07', '51.5'],
+  zoom: 9
 });
+
+map.addControl(new mapboxgl.NavigationControl());
+console.log(map)
